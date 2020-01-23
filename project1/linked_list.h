f7baa57a72711550feb0391749d3b29eb545a244
AJ Stein <as169117@ohio.edu>
2020-01-23 18:06:43 -0500

    First Working on it.
    I have added the menu, and tested all the functions except reverse. In which they all work. Things to do: Add reverse, command line arguments

--- project1/linked_list.h
+++ project1/linked_list.h
@@ -1,22 +1,32 @@
-template <typename T>
+/*
+* AJ Stein
+* Ohio University CS3610 Project 1
+* 1-16-20
+* We are to write a program that takes some numbers from the command line, and then print them, add to them, delete them, or reverse the order
+*/
+#ifndef LINKEDL
+#define LINKEDL
+
+template<class T>
 class LinkedList {
+  public:
+    LinkedList() : head(NULL) {} // this is the same as head = NULL;
+    ~LinkedList();
 
-public:
-  LinkedList() : head(NULL) {}
-  ~LinkedList();
+    void push_front(const T data);
+    void pop_front();
+    void reverse();
+    void print() const;
 
-  void push_front(const T data);
-  void pop_front();
-  void reverse();
-  void print() const;
+  private:
+    struct ListNode {
+      ListNode(const T data) : data(data), next(NULL) {}
 
-private:
-  struct ListNode {
-    ListNode(const T data) : data(data), next(NULL) {}
-
-    T data;
-    ListNode* next;
+      T data; 
+      ListNode* next; 
   };
 
   ListNode* head;
 };
+#include "LL.template"
+#endif
---
+++ project1/main.cc
@@ -0,0 +1,80 @@
+/*
+* AJ Stein
+* Ohio University CS3610 Project 1
+* 1-16-20
+* We are to write a program that takes some numbers from the command line, and then print them, add to them, delete them, or reverse the order
+*/
+#include <iostream>
+#include "linked_list.h"
+using namespace std;
+/* Program Notes:
+* Need to work on getting command line arguements working
+* Need to get reverse working.
+* - A, D, P, Q options work
+*/
+// Functions:
+char menu(); // Menu for the user
+
+//*********************************************
+int main(int argc, char** argv) {
+  LinkedList<int> list;
+
+  cout << "Please do lower case only!";
+
+  // Two variables for the users choices
+  int add_num;
+  char choice;
+  // While statement continues to run program until Q is entered
+  while(choice != 'q'){
+    choice = menu(); // takes the users choice, duh
+    
+    switch(choice){
+//*********************************************
+      // default status of you done messed up with entering an option!
+      default:
+        cout << "\nThis isnt a valid option, retry!" << endl;
+        break;
+//*********************************************
+      // does literally nothing besides hurt performance probably
+      case 'q':
+        break;
+//*********************************************
+      // Prints List
+      case 'p':
+        list.print();
+        break;
+//*********************************************
+      // Adds to front of list
+      case 'a':
+        cout << "\nPlease enter a number:";
+        cin >> add_num;
+        list.push_front(add_num);
+        break;
+//*********************************************
+      // Deletes first value in list
+      case 'd':
+        list.pop_front();
+        break;
+//*********************************************
+    } // end of switch
+  } // end of while
+  cout << "\nYou have quit the program, have a great day!" << endl;
+
+return 0;
+} // end of main
+
+//*********************************************
+char menu(){
+  char choice;
+  int num = 0;
+  // console menu:
+  cout << "\na: Add the integer i to the front of the list. (push front)." << endl;
+  cout << "d: Delete the first element of the list. (pop front)" << endl;
+  cout << "r: reverse the list." << endl;
+  cout << "p: Print the data value of each node in the list." << endl;
+  cout << "q: Quit the program." << endl;
+  cout << "Please type your choice:";
+  cin >> choice;
+  return choice;
+}
+//*********************************************
