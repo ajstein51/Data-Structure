b4893926d48f26d38db89f5446777a5ac5702b4d
AJ Stein <as169117@ohio.edu>
2020-02-18 09:33:08 -0500

    Finished program.

--- Project2/project/source/knights_tour.cc
+++ Project2/project/source/knights_tour.cc
@@ -38,10 +38,11 @@ void KnightsTour::print() {
 //                      the current position row, col.
 //    int num_moves -   Number of valid moves found. Corresponds
 //                      to the sizes of row_moves and col_moves.
-void KnightsTour::get_moves(int row, int col, int row_moves[], int col_moves[], int& num_moves){
+
+void KnightsTour::get_moves(int row, int col, int row_moves[], int col_moves[], int& num_moves){ 
   // Checks all possible moves (8) with the isvalid function, then proceeds to place the number there
-//-----------------------------------------------------------
   int i = 0;
+//-----------------------------------------------------------
   if(isvalid(row + 2, col + 1) == true){
     row_moves[i] = row + 2, col_moves[i] = col + 1;
     num_moves++;
@@ -90,13 +91,7 @@ void KnightsTour::get_moves(int row, int col, int row_moves[], int col_moves[], 
     i++;
   }
 //-----------------------------------------------------------
-  if(isvalid(row + 2, col - 1 ) == true){
-    row_moves[i] = row + 2, col_moves[i] = col - 1;
-    num_moves++;
-    i++;
-  } 
-//-----------------------------------------------------------
-} // function end
+} // end of function
 
 // Function: move() --> Recursive
 //     int row        - Current row position of knight.
@@ -105,29 +100,33 @@ void KnightsTour::get_moves(int row, int col, int row_moves[], int col_moves[], 
 //                      Stored in board at position
 //                      row, col.
 //     int& num_tours - Total number of tours found.
-void KnightsTour::move(int row, int col, int& m, int& num_tours){
+
+void KnightsTour::move(int row, int col, int& m, int& num_tours){ 
   // Starting Values:
   m++;
   board[row][col] = m;
-  int row_moves[8]; //= {  2, 1, -1, -2, -2, -1,  1,  2 };  // These are the only possible moves
+  int row_moves[8]; //= {  2, 1, -1, -2, -2, -1,  1,  2 };  These are the only possible moves
   int col_moves[8]; //= {  1, 2,  2,  1, -1, -2, -2, -1 }; 
   int num_moves = 0;
   get_moves(row, col, row_moves, col_moves, num_moves);
-  // If there is no possible moves left:
-  if(m == 25){ 
+  
+  // If no moves left:
+  if(m == 25){
     num_tours++; 
     print();
   }
+  
   for(int i = 0; i < num_moves; i++){
     move(row_moves[i], col_moves[i], m, num_tours);
   }
-  // backtrack decrement M and undo the move (set back to 0)
+  
+  // Backtrack, decrements 'm' and undo the last move (set equal to 0):
   m--;
   board[row][col] = 0;
 }
 
 bool KnightsTour::isvalid(int x, int y){
-// Checks to see if its on the board and if the peice is 0 (not been to yet).
+  // Checks to see if the move is within the board and then checks to see if its been done already:
   if((x >= 0 && x < 5 && y >= 0 && y < 5) && board[x][y] == 0){
       return true;
   }
