81f3048682401eb0520a2ab2ca8f8e36c9004ea4
AJ Stein <as169117@ohio.edu>
2020-02-22 16:30:58 -0500

    Inserting New project
    Pushing the new project (3) to github.

---
+++ project3/source/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "huffman_tree.h"
+using namespace std;
+
+int main(int argc, char** argv) {
+  // Create a HuffmanTree object and read the input messages into the
+  // HuffmanTree construct function. Next, print the encoded message.
+  // Finally, destruct the huffman tree and move on to the next test case.
+}
